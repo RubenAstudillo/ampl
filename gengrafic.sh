8cd61d6cdcf5410006c25ab03ae0f4bf04268875
Ruben Astudillo <ruben.astud@gmail.com>
2016-11-16 00:31:53 -0300

    N300 N350 y ploteo de K sobre N

--- gengrafic.sh
+++ gengrafic.sh
@@ -1,4 +1,4 @@
-for n in {100,150,200,250}
+for n in {100,150,200,250,300,350}
 do
   for k in $(seq 5 5 50)
   do
@@ -7,5 +7,15 @@ do
   done
   gnuplot -c ../grafica-N.plt
   mv grafico.png graficos/N${n}.png
-  mv datos.dat graficos/datos-${n}.dat
+  mv datos.dat graficos/datos-N${n}.dat
 done
+
+k=25
+for n in {100,150,200,250,300,350}
+do
+  dat=$(gawk -v N=${n} -f ../extract.awk log/res-N${n}-K${k}.txt)
+  echo $dat >> datos.dat
+done
+gnuplot -c ../grafica-K.plt
+mv grafico.png graficos/K${k}.png
+mv datos.dat graficos/datos-K${k}.dat
--- grafica-K.plt
+++ grafica-K.plt
@@ -1,6 +1,6 @@
 set term png
 set output "grafico.png"
-set xlabel "K"
-set title "Porcentaje de error"
+set xlabel "N"
+set title "Porcentaje de error K=25"
 plot "datos.dat" using 1:2 title "Nuestro", \
      "datos.dat" using 1:3 title "Relajado"
